048509bd5892038a468cebcf99172cf839fd116a
Stain04 <stain.hs04@gmail.com>
2026-04-27 04:19:22 +0300

    Update dashboard.js

--- api/customer/dashboard.js
+++ api/customer/dashboard.js
@@ -1,4 +1,4 @@
-import { getKV, verifyToken } from '../_lib/auth.js';
+import { getKV, verifyToken } from '../_lib/authfun.js';
 
 export default async function handler(req, res) {
   if (req.method !== 'GET') return res.status(405).json({ error: 'Method not allowed' });
